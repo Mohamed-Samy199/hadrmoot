4db026d7374fe45b8d975de062c2b80c9e7105ec
Mohamed-Samy199 <https://github.com/Mohamed-Samy199>
2025-09-04 15:02:07 +0300

    app
    app

--- src/utils/hostUrl.ts
+++ src/utils/hostUrl.ts
@@ -1,10 +1,10 @@
 import { safeLocalStorage } from "@/utils/safeLocalStorage";
 
 // ✅ رابط الـ Backend
-// export const hostUrl = "https://restaurant-backend-778b.vercel.app";
+export const hostUrl = "https://restaurant-backend-778b.vercel.app";
 
 // ✅ الهيدرز اللي هيتبعت مع كل request
-export const hostUrl = "https://restaurant-backend-iz1gb9ph0-restaurants-projects-a64d6409.vercel.app";
+// export const hostUrl = "https://restaurant-backend-iz1gb9ph0-restaurants-projects-a64d6409.vercel.app";
 
 export const headers = {
   "Content-Type": "application/json; charset=utf-8",
